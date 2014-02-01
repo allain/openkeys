c63025eb4dc0e95bb0388cf52d7655df6500ce22
Allain Lalonde <allain.lalonde@gmail.com>
2014-02-01 13:11:16 -0500

    Adding base openkeys.js file

---
+++ public/openkeys.js
@@ -0,0 +1,11 @@
+(function($) {
+  if ($ === undefined) {
+    if (console && console.error) {
+      console.error("need Zepto or jQuery to do AJAX calls");
+    }
+
+    return;
+  }
+
+  console.log("TODO: Add logic here for providing a simple nestable db api");
+})(window.Zepto || window.jQuery);
